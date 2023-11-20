e322ea0ffe0ab6f02de82eec26b2e69f4094a0a3
Mikołaj Baran (Uczeń) <mbaran343@technikumkreatywne.pl>
2023-11-20 14:10:14 +0100

    something

--- sweeper.js
+++ sweeper.js
@@ -1,7 +1,7 @@
 const rows = 3
 const columns = 3
-board = []
-value = 0
+let board = Array(rows).fill(0).map(()=> Array(columns).fill(0))
+let value = 0
 for(i=0;i < rows;i++)
 {
     for(j=0;j<columns;j++)
